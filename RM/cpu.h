491be8fde65dd05e147d57f0bf4280916543041d
lume <lukas.michnevic@protonmail.com>
2020-03-02 15:32:19 +0200

    Quick follow up

--- RM/cpu.h
+++ RM/cpu.h
@@ -7,10 +7,7 @@
 class Cpu
 {
     public:
-        Cpu()
-        {
-            
-        }
+        Cpu(){}
         void ShowRam(); // Show the contents of the RAM
         void LoadProgram(std::vector<int> machineCode);// Load the Program machine code to the memory
         void ExecuteProgram(); // Execute the loaded program
@@ -49,6 +46,3 @@ class Cpu
         void OP_DEC();
         void UNDEFINED();
 };
-void ShowRam(); // Show the contents of the RAM
-void LoadProgram(std::vector<int> machineCode);// Load the Program machine code to the memory
-void ExecuteProgram(); // Execute the loaded program
